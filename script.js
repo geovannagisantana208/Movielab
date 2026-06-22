60ec52361348fcbc5cb86096dba0e03e567ef916
geovannagisantana208 <geovannagisantana208@gmail.com>
2026-06-22 08:45:01 -0300

    Add files via upload
    22/06

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function enviar() {
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let mensagem = document.getElementById("mensagem").value;
+function mensagem() {
+    alert("Bem-vindo ao MovieLab!");
+}
 
-    if (nome === "" || email === "" || mensagem === "") {
-        document.getElementById("msg").innerText =
-            "Por favor, preencha todos os campos.";
-        return;
+function toggle(botao) {
+
+    let sinopse = botao.nextElementSibling;
+
+    if (sinopse.style.display === "block") {
+        sinopse.style.display = "none";
+    } else {
+        sinopse.style.display = "block";
     }
 
-    document.getElementById("msg").innerText =
-        "Mensagem enviada com sucesso, " + nome + "! Obrigado por entrar em contato.";
-
-    document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("mensagem").value = "";
 }
